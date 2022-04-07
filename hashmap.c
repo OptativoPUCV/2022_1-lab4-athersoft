6eee1ed3dbad24f5e61ef59febe4042b4777e95d
athersoft <>
2022-04-07 16:15:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -80,7 +80,7 @@ HashMap * createMap(long capacity) {
 void eraseMap(HashMap * map,  char * key) {    
   long index = hash(key, map-> capacity);
   map -> buckets[index] -> key = NULL;
-  size--;
+  map->size--;
 
 }
 
