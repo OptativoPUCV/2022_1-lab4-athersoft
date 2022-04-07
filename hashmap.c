c69794601a219733e597f334389b6dcd2c489693
athersoft <>
2022-04-07 16:05:04 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -87,7 +87,7 @@ Pair * searchMap(HashMap * map,  char * key) {
   if(map -> buckets[index] == NULL){
     return NULL;
   }else{
-    while(map -> bucket[index] -> key != key && map -> buckets[index] != NULL){
+    while(map -> buckets[index] -> key != key && map -> buckets[index] != NULL){
       index++;
       if(map -> buckets[index] == NULL){
         return NULL;
