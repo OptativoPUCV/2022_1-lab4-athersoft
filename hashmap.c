69b32030dacfb3c8262c2b58c406feb7b2450d26
athersoft <>
2022-04-07 15:40:52 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -73,7 +73,7 @@ HashMap * createMap(long capacity) {
     map -> size = 0;
     map -> capacity = capacity;
     map -> current = -1;
-    return NULL;
+    return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
