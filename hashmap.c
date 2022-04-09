d7fed1a0ddbe67a7c9f52e04f4dadf705eb8ebc3
athersoft <>
2022-04-09 22:53:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -81,12 +81,13 @@ void eraseMap(HashMap * map,  char * key) {
   long index = hash(key, map-> capacity);
   while(is_equal(map ->buckets[index]->key, key) == 0){
     index++;
-    if(map -> buckets[index] == NULL){
-        break;
-    }
+
   }
-  map -> buckets[index] -> key = NULL;
-  map->size--;
+  if(map -> buckets[index] -> key == key){
+      map -> buckets[index] -> key = NULL;
+      map->size--;
+  }
+  
 
 }
 
