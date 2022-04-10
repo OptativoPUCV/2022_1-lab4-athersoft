9c1d073611bdd1f649aa43c10987b60c0834bac0
athersoft <>
2022-04-10 00:12:25 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -105,7 +105,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-  for(int i = 0; i< map->capacity; i++){
+  for(int i = 0; i< map->capacity-1; i++){
     if(map -> buckets[i] -> key != NULL){
       return map -> buckets[i];
     }
